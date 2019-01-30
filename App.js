21537dea6ab9c449c7bfe535e844d3a80a01e0e4
Brian Duong <brianduongh@gmail.com>
2019-01-30 16:46:44 -0600

    added apollo

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo'
 import TabNavigator from './Navigation.js';
+import client from './apollo';
 
 export default class App extends Component {
   render() {
     return (
-      <TabNavigator />
+      <ApolloProvider client={client}>
+        <TabNavigator />
+      </ApolloProvider>
     );
   }
 }
