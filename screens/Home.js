11efc82f269a436f5b7cebb9f446b2f9b2652ca1
Brian Duong <brianduongh@gmail.com>
2019-01-30 16:18:32 -0600

    added native-elements

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
+import { Button, Card } from 'react-native-elements';
+import axios from 'axios';
 
 export default class HomeScreen extends React.Component {
+  state = {
+    books: ''
+  }
+
+  componentDidMount() {
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=9780060904791+isbn&key=AIzaSyCekaWwYYUa61_90Z8UVPjdoYvVAWTkqhI')
+    .then(response => {
+      this.setState({ books: response.data.items[0].volumeInfo.title })
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>home!</Text>
-      </View>
+      <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+        <Card>
+          <View>
+            <Text>{this.state.books}</Text>
+          </View>
+        </Card>
+      </SafeAreaView>
     );
   }
 }
